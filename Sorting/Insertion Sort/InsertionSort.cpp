d0f61f5ce4a73d79aaa6e9261230c46d62bf6390
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-06-08 23:36:13 +0530

    insertion sort

---
+++ Sorting/Insertion Sort/InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
